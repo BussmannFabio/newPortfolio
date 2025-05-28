4b1b935b37dc3b410e0c74c93382699ebb809c4a
Fabio <fabio12513@gmail.com>
2025-05-28 09:08:16 -0300

    particulas adicionadas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,58 @@
 console.log('Portfolio Carregado!')
+
+tsParticles.load("tsparticles", {
+
+    fpsLimit: 100,
+    particles: {
+        number: {
+            value: 60,
+            density: {
+                enable: true,
+                area: 800
+            }
+        },
+        color: {
+            value: "#10b981" 
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 0.6
+        },
+        size: {
+            value: 6
+        },
+        move: {
+            enable: true,
+            speed: 1,
+            direction: "none",
+            outModes: "bounce"
+        }
+    },
+    interactivity: {
+        events: {
+            onHover: {
+                enable: true,
+                mode: "repulse"
+            },
+            resize: true
+        },
+        modes: {
+            repulse: {
+                distance: 100
+            }
+        }
+    },
+    detectRetina: true
+});
+
+  const btn = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", () => {
+    btn.classList.toggle("hidden", window.scrollY < 300);
+  });
+
+  btn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
