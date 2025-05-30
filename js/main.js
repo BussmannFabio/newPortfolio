92a624c72cb596e9fc863a605e255028a7663d14
fabiobussmann <fabio12513@gmail.com>
2025-05-30 10:46:58 -0300

    new footer

--- js/main.js
+++ js/main.js
@@ -1,58 +1,57 @@
-console.log('Portfolio Carregado!')
+console.log("Portfolio Carregado!");
 
 tsParticles.load("tsparticles", {
-
-    fpsLimit: 100,
-    particles: {
-        number: {
-            value: 80,
-            density: {
-                enable: true,
-                area: 800
-            }
-        },
-        color: {
-            value: "#10b981" 
-        },
-        shape: {
-            type: "circle"
-        },
-        opacity: {
-            value: 0.6
-        },
-        size: {
-            value: 3
-        },
-        move: {
-            enable: true,
-            speed: 1,
-            direction: "none",
-            outModes: "bounce"
-        }
+  fpsLimit: 100,
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        area: 800,
+      },
     },
-    interactivity: {
-        events: {
-            onHover: {
-                enable: true,
-                mode: "repulse"
-            },
-            resize: true
-        },
-        modes: {
-            repulse: {
-                distance: 100
-            }
-        }
+    color: {
+      value: "#10b981",
     },
-    detectRetina: true
+    shape: {
+      type: "circle",
+    },
+    opacity: {
+      value: 0.6,
+    },
+    size: {
+      value: 3,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      outModes: "bounce",
+    },
+  },
+  interactivity: {
+    events: {
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      repulse: {
+        distance: 100,
+      },
+    },
+  },
+  detectRetina: true,
 });
 
-  const btn = document.getElementById("backToTop");
+const btn = document.getElementById("backToTop");
 
-  window.addEventListener("scroll", () => {
-    btn.classList.toggle("hidden", window.scrollY < 300);
-  });
+window.addEventListener("scroll", () => {
+  btn.classList.toggle("hidden", window.scrollY < 300);
+});
 
-  btn.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
+btn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
